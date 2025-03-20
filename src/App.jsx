ff4a52a8a5c44f0007408ba48fc48731a02dbd99
chris <christopher.peters@itpeoplecorp.com>
2025-03-20 09:51:31 +0530

    Navigation with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CategoryContainer from "./components/CategoryContainer/CategoryContainer";
+import { Routes, Route, Link, Outlet } from "react-router";
 function App() {
   const categories = [
     {
@@ -32,11 +33,65 @@ function App() {
         "https://res.cloudinary.com/dmlsjxqkt/image/upload/v1741606029/men_u3t7qh.png",
     },
   ];
-  return (
-    <>
+  const Navigation = () => {
+    return (
+      <>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/hats">Hats</Link>
+          </li>
+          <li>
+            <Link to="/jackets">Jackets</Link>
+          </li>
+        </ul>
+        <Outlet />
+      </>
+    );
+  };
+  const Home = () => {
+    return (
       <div>
         <CategoryContainer categories={categories} />
       </div>
+    );
+  };
+  const Hats = () => {
+    return (
+      <div>
+        <h1>Welcome to the Hats page</h1>
+      </div>
+    );
+  };
+  const Jackets = () => {
+    return (
+      <div>
+        <h1>Welcome to the Jackets page</h1>
+      </div>
+    );
+  };
+  const NotFound = () => {
+    return (
+      <div>
+        <h1>The Page you requested is not found</h1>
+        <p>
+          Click <Link to="/">Home</Link> to go back to the Home Page
+        </p>
+      </div>
+    );
+  };
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="hats" element={<Hats />} />
+          <Route path="jackets" element={<Jackets />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </>
   );
 }
